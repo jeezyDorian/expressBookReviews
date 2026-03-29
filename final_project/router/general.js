c73d4a70441648aa5875bf807cb56252abfecf80
Imants <151896039+jeezyDorian@users.noreply.github.com>
2026-03-29 20:00:10 +0300

    task11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,36 +33,18 @@ public_users.get('/', function (req, res) {
 });
 
 // Get book details based on ISBN
-//public_users.get('/isbn/:isbn',function (req, res) {
-//  const isbn = req.params.isbn;
-//  const book = books[isbn];
-//  if (book) {
-//    return res.status(200).json(book);
-//  } else {
-//    return res.status(404).json({message: `Book with ISBN ${isbn} not found`});
-//  }
-// });
-
- public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  const bookPromise = new Promise((resolve, reject) => {
-    const book = books[isbn];
-    if (book) {
-      resolve(book);
-    } else {
-      reject(new Error(`Book with ISBN ${isbn} not found`));
-    }
+  const promise = new Promise((resolve, reject) => {
+    axios.get(`http://localhost:5000/isbn/${isbn}`)
+      .then(response => resolve(response.data))
+      .catch(error => reject(error));
   });
-  
-  bookPromise
-    .then(book => {
-      res.status(200).json(book);
-    })
-    .catch(error => {
-      res.status(404).json({ message: error.message });
-    });
+  promise
+    .then(data => res.status(200).json(data))
+    .catch(error => res.status(404).json({ message: `Error fetching book details with ISBN ${isbn}`, error: error.message }));
 });
-  
+
 // Get book details based on author
 //public_users.get('/author/:author', function (req, res) {
 //  const author = req.params.author;
