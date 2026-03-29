011eb6b29f444847c7c8a1afbd7805bf12259baf
Imants <151896039+jeezyDorian@users.noreply.github.com>
2026-03-29 16:04:51 +0300

    implemented get by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,9 +36,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const matchingBooks = [];
+  const bookKeys = Object.keys(books);
+  
+  bookKeys.forEach(key => {
+      if (books[key].author.toLowerCase() === author.toLowerCase()) {
+      matchingBooks.push(books[key]);
+    }
+  });
+  
+  if (matchingBooks.length > 0) {
+    return res.status(200).json(matchingBooks);
+  } else {
+    return res.status(404).json({message: `No books found by author ${author}`});
+  }
 });
 
 // Get all books based on title
