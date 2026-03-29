940c48968cb6f350e8c8d15770613278d201429a
Imants <151896039+jeezyDorian@users.noreply.github.com>
2026-03-29 17:13:48 +0300

    add review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -49,8 +49,25 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const review = req.body.review;  // Changed to body
+  const username = req.session.authorization.username;
+  
+  if (!isbn || !review || !username) {
+    return res.status(400).json({message: "ISBN, review (body), and login required"});
+  }
+  
+  if (!books[isbn]) {
+    return res.status(404).json({message: `Book with ISBN ${isbn} not found`});
+  }
+  
+  if (!books[isbn].reviews) {
+    books[isbn].reviews = {};
+  }
+  
+  books[isbn].reviews[username] = review;
+  
+  return res.status(201).json({message: `Review for ${isbn} updated`});
 });
 
 module.exports.authenticated = regd_users;
