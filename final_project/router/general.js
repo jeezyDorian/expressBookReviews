c0da3d117a4e58c85a08dfd10d5c385c8be7ab25
Imants <151896039+jeezyDorian@users.noreply.github.com>
2026-03-29 18:34:35 +0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,45 +21,20 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-
-//public_users.get('/',function (req, res) {
-//  res.send(JSON.stringify(books,null,4));
-//});
-
+public_users.get('/',function (req, res) {
+  res.send(JSON.stringify(books,null,4));
+});
 
 // Get book details based on ISBN
-//public_users.get('/isbn/:isbn',function (req, res) {
-//  const isbn = req.params.isbn;
-//  const book = books[isbn];
-//  if (book) {
-//    return res.status(200).json(book);
-//  } else {
-//    return res.status(404).json({message: `Book with ISBN ${isbn} not found`});
-//  }
-// });
-
-// Get book details based on ISBN using async-await with Axios
-public_users.get('/isbn/:isbn', async (req, res) => {
+public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  try {
-    // Wrap local books lookup in a Promise to simulate async behaviour
-    const getBookByIsbn = (isbn) => {
-      return new Promise((resolve, reject) => {
-        const book = books[isbn];
-        if (book) {
-          resolve(book);
-        } else {
-          reject(new Error(`Book with ISBN ${isbn} not found`));
-        }
-      });
-    };
-
-    const book = await getBookByIsbn(isbn);
+  const book = books[isbn];
+  if (book) {
     return res.status(200).json(book);
-  } catch (error) {
-    return res.status(404).json({ message: error.message });
+  } else {
+    return res.status(404).json({message: `Book with ISBN ${isbn} not found`});
   }
-});
+ });
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
