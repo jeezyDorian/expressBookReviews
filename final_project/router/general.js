ecc47687dfe1fe7bf7afe6aa4648c9a4a5ac2555
Imants <151896039+jeezyDorian@users.noreply.github.com>
2026-03-29 18:00:23 +0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -44,7 +44,6 @@ public_users.get('/', (req, res) => {
 });
 
 // Get book details based on ISBN
-
 //public_users.get('/isbn/:isbn',function (req, res) {
 //  const isbn = req.params.isbn;
 //  const book = books[isbn];
@@ -54,6 +53,29 @@ public_users.get('/', (req, res) => {
 //    return res.status(404).json({message: `Book with ISBN ${isbn} not found`});
 //  }
 // });
+
+// Get book details based on ISBN using async-await with Axios
+public_users.get('/isbn/:isbn', async (req, res) => {
+  const isbn = req.params.isbn;
+  try {
+    // Wrap local books lookup in a Promise to simulate async behaviour
+    const getBookByIsbn = (isbn) => {
+      return new Promise((resolve, reject) => {
+        const book = books[isbn];
+        if (book) {
+          resolve(book);
+        } else {
+          reject(new Error(`Book with ISBN ${isbn} not found`));
+        }
+      });
+    };
+
+    const book = await getBookByIsbn(isbn);
+    return res.status(200).json(book);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
